2c19712cc159941908045b6b1aa2ce64511c7f72
leonardobls <leonardobitencourt2020@gmail.com>
2024-05-07 22:30:26 -0300

    initial commit

---
+++ InsightHub/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsightHub.Controllers;
+
+public class LoginController : Controller
+{
+    // private readonly ILogger<LoginController> _logger;
+
+    public LoginController()
+    {
+        // _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+}
